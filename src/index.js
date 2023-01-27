874b00119f9adcae62542aeae38c45fbd88d9ab3
Ola <ohagenbendixen@gmail.com>
2023-01-27 07:38:58 +0100

    Setup the algorithm to change tab

--- src/index.js
+++ src/index.js
@@ -6,12 +6,41 @@ loadPage();
 // loadMenu();
 // loadAbout();
 
-const switchTab = () => {
+const switchTab = (() => {
   const tabs = document.querySelectorAll('.tab');
+  let currentTab = 'Home';
+  const homeContent = document.querySelector('#home-content');
+  const menuContent = document.querySelector('#menu-content');
+  const aboutContent = document.querySelector('#about-content');
+
+  const removeHomeContent = () => {
+    homeContent.style.display = 'none';
+  };
+
+  const setCurrentTab = (tab) => {
+    if (tab === menuContent) {
+      loadMenu();
+    }
+    if (tab === aboutContent) {
+      loadAbout();
+    }
+    if (tab === homeContent) {
+      loadPage();
+    }
+
+    tab.style.display = 'block';
+  };
 
   tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
-        
+      if (currentTab === 'Home' && tab.textContent === 'Home') {
+        return 1;
+      }
+      if (currentTab === 'Home' && tab.textContent === 'Menu') {
+        currentTab = 'Menu';
+        removeHomeContent();
+        setCurrentTab(menuContent);
+      }
     });
   });
-};
+})();
